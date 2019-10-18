2296cfeef4340fbce9ad89392a0c470e4ae6ee61
Emelyn-Segovia <esegovia09@hotmail.com>
2019-10-18 13:34:57 -0500

    floating Action Button con menú 1

--- app/src/main/java/com/sistemas/evaluacion/MainMenu.java
+++ app/src/main/java/com/sistemas/evaluacion/MainMenu.java
@@ -2,8 +2,6 @@ package com.sistemas.evaluacion;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,12 +10,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.sistemas.evaluacion.entidades.datosGenerales;
 import com.sistemas.evaluacion.entidades.datosEntrevistador;
+import com.sistemas.evaluacion.entidades.datosGenerales;
+import com.github.clans.fab.FloatingActionMenu;
 
 import java.util.ArrayList;
 
@@ -33,26 +33,17 @@ public class MainMenu extends AppCompatActivity
     public String tamaño="";
     ListToCSV convierte=new ListToCSV();
     //endregion
-
+    FloatingActionMenu actionMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-
         //region Navigation Drawer
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
-                Intent intent= new Intent(view.getContext(), entrevista.class);
-                startActivity(intent);
-            }
-        });
+        actionMenu=(FloatingActionMenu)findViewById(R.id.fabPrincipal);
+        actionMenu.setClosedOnTouchOutside(true); //Boton FloatingMenu
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -93,6 +84,13 @@ public class MainMenu extends AppCompatActivity
             }
         });
         //endregion
+
+    }
+
+    public void clicSubMenu1(View View){ //BotonFloatingMenu
+
+        Intent intent= new Intent(this, entrevista.class);
+        startActivity(intent);
 
     }
 
