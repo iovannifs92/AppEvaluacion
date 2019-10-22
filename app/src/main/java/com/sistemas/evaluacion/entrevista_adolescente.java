16ab5a132790f7af71ea45ffad567f5bf7c55e5a
Emelyn-Segovia <esegovia09@hotmail.com>
2019-10-22 09:21:45 -0500

    Actualizacion

--- app/src/main/java/com/sistemas/evaluacion/entrevista_adolescente.java
+++ app/src/main/java/com/sistemas/evaluacion/entrevista_adolescente.java
@@ -3,6 +3,7 @@ package com.sistemas.evaluacion;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckedTextView;
 import android.widget.EditText;
@@ -13,35 +14,56 @@ import android.widget.TextView;
 public class entrevista_adolescente extends AppCompatActivity {
     //region variables globales
     private MyOpenHelper db;
+
     //endregion
 
     //region TextView
-    private TextView tvNc;
-
-
+    private TextView tvNom,tvRa,tvPOa,tvNa,tvEa,tvFNa,tvSex;
+    //endregion
+    //regio boolean
+    private boolean pR=false;
     //endregion
 
     //region EditText
-    private EditText txtNc, txtFecha, txtNe, txtFolioA;
+    private EditText txtNc, txtFecha, txtNe, txtFolioA ,txtNpa1, txtNpa2, txtNpa3, txtNpa4, txtRa1, txtRa2, txtRa3, txtRa4, txtPOa1, txtPOa2, txtPOa3, txtPOa4, txtNa, txtEa, txtFNa, txtCURPa, txtLNa, txtEDOa, txtMa, txtLa, txtNAa ;
+    //Ficha familiar Domicilio
+    private EditText txtCa, txtNOa, txtCOLa, txtCPa, txtMUa, txtEDO2a, txtPa, txtTa, txtDALa1, txtDALa2, txtDALa3, txtDALa4, txtDATa1, txtDATa2, txtDATa3, txtDATa4;
+    //Datos familiares
+    private EditText txtNDFa1,txtNDFa2,txtNDFa3,txtNDFa4,txtRDFa1, txtRDFa2, txtRDFa3, txtRDFa4, txtEDFa1, txtEDFa2, txtEDFa3, txtEDFa4, txtTDFa1, txtTDFa2, txtTDFa3, txtTDFa4, txtVDF1, txtVDF2, txtVDF3, txtVDF4, txtCQDFa,txtRDFa,txtLDFa;
+    //Dependientes economicos
+    private EditText txtNDEa1,txtNDEa2,txtNDEa3,txtNDEa4, txtRDEa1,txtRDEa2,txtRDEa3,txtRDEa4,txtTDEa1,txtTDEa2,txtTDEa3,txtTDEa4,txtADE1,txtADE2,txtADE3,txtADE4;
+    //Viculos comunitarios
+    private EditText txtEAa,txtNEa,txtDEa,txtTea,txtNIEa,txtUGa,txtNEa1,txtNEa2,txtNEa3,txtNEa4,txtLE1,txtLE2,txtLE3,txtLE4,txtGC1,txtGC2,txtGC3,txtGC4;
+    //Historial laboral
+    private EditText txtTAa,txtNTa,txtDTa,txtTTa,txtATa,txtJTa,txtNT1,txtNT2,txtNT3,txtNT4,txtLT1,txtLT2,txtLT3,txtLT4,txtAT1,txtAT2,txtAT3,txtAT4,txtTT1,txtTT2,txtTT3,txtTT4;
+    //actividades Extraescolares
+    private EditText txtA1,txtA2,txtA3,txtA4,txtL1,txtL2,txtL3,txtL4,txtC1,txtC2,txtC3,txtC4;
+    //Consideraciones Excepcionales
+    private EditText txtCual;
     //endregion EditText
 
     //reguin Sppiner
-    private Spinner a;
+    private Spinner sPSex;
     //endregion
     //region LinearLayout
-    private LinearLayout ab;
+    private LinearLayout llPersonasPresentes;
     //endregionLinearLayout
     //region Buttons
-    private Button btnGuardarA;
+    private Button btnGuardarA,btnPersonasPresentes;
 
     //endregion Buttons
     //regionCheckedTextView
     CheckedTextView s;
     //endregion
-
+    //Region Spinner
+    private Spinner sP1a;
     //Region String
     private String r1A, r2A, r3A, r4A;
+    private String [] nosi={"No", "Si"};
+
     //endregion String
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +71,10 @@ public class entrevista_adolescente extends AppCompatActivity {
         setContentView(R.layout.activity_entrevista_adolescente);
         db=new MyOpenHelper(this);
 
+            //Regio sP1a Spinner
+              sP1a=(Spinner) findViewById(R.id.sP1a);
+              sP1a.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,nosi));
+            //endregion
 
             btnGuardarA=(Button) findViewById(R.id.btnGuardarA);
             btnGuardarA.setOnClickListener(new View.OnClickListener() {
@@ -73,6 +99,27 @@ public class entrevista_adolescente extends AppCompatActivity {
 
                 }
             });
+
+        //region 1. PERSONAS PRESENTES EN LA ENTREVISTA DEL IMPUTADO
+        btnPersonasPresentes=(Button) findViewById(R.id.btnPersonasPresentes);
+        llPersonasPresentes=(LinearLayout) findViewById(R.id.llPersonasPresentes);
+        btnPersonasPresentes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!pR){
+                    llPersonasPresentes.setVisibility(View.VISIBLE);
+                    pR=true;
+                }
+                else{
+                    llPersonasPresentes.setVisibility(View.GONE);
+                    pR=false;
+                }
+            }
+
+
+            //endregion
+        });
+
 
     }
 }
