b83d904fc1b32499f2e7e275dd874189cee65078
Emelyn-Segovia <esegovia09@hotmail.com>
2019-10-30 14:31:11 -0600

    Actualizacion diseño y variables listo 2.3

---
+++ app/src/main/java/com/sistemas/evaluacion/infoCasoAdolescente.java
@@ -0,0 +1,13 @@
+package com.sistemas.evaluacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class infoCasoAdolescente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_caso_adolescente);
+    }
+}
